a88f93e2efbe4c8ed31ea743e0d1f123c002ac56
‘samahAbujwaied’ <samah.abujweed@yahoo.com>
2021-04-25 16:06:55 +0300

    add

---
+++ app.js
@@ -0,0 +1,17 @@
+
+       'use strict';
+
+       let nam = prompt("Enter Your Name Please ");
+       let age = prompt("Enter Your Age");
+       let country= prompt("Where are you from" );
+       let color= prompt("What color you prefear" );
+       
+       
+         console.log("<p> Welcome " + nam +" your Age is " + age + " You are  from " + country + " Best regards to you </p> " )
+          document.write("<p> Welcome " + nam +" your Age is " + age + " You are  from " + country + " Best regards to you </p> " );
+     
+            document.body.style.background = color;
+            
+
+         
+  
